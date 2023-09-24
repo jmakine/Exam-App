dde27ab066054a0e7af1126433ebc94716e4bc21
Jenni Mäkinen <jenni.makinen70@gmail.com>
2023-09-24 18:06:08 +0300

    get_exams_and_point for all and for one user added to answers.py

--- answers.py
+++ answers.py
@@ -23,13 +23,11 @@ def check_answer(users_answer, correct_answer):
     else:
         return False
 
-# returns correct answer and points
 def get_answer_and_points(question_id):
     sql = "SELECT answer, points from questions WHERE id=:question_id"
     result = db.session.execute(text(sql), {"question_id":question_id}).fetchone() # ).fetchone()  #, 
     return result
 
-# question_id, exam_id, answer
 def answer_question(question_id, exam_id, answer):
     user_id = users.user_id()
     user_exam_id = get_user_exam_id(user_id, exam_id)
@@ -62,3 +60,27 @@ def submitted_answers(exam_id, user_id):
         LEFT JOIN users_exams ue on ua.user_exam_id=ue.id \
         WHERE eq.exam_id=:exam_id AND ue.user_id=:user_id"
     return db.session.execute(text(sql), {"exam_id":exam_id, "user_id":user_id}).fetchall()
+
+def get_exams_and_points(user_id):
+    sql = "SELECT eq.exam_id, SUM(ua.points_received) as points_received, TO_CHAR(ue.exam_finished, 'YYYY/MM/DD HH24:MM:SS') as exam_finished, TO_CHAR(ue.exam_started, 'YYYY/MM/DD HH24:MM:SS') as exam_started FROM users_answers ua \
+        LEFT JOIN exams_questions eq on eq.id=ua.exams_question_id \
+        LEFT JOIN questions q on q.id=eq.question_id \
+        LEFT JOIN users_exams ue on ua.user_exam_id=ue.id \
+        WHERE ue.user_id=:user_id \
+        GROUP BY eq.exam_id, exam_finished, exam_started"
+    return db.session.execute(text(sql), {"user_id":user_id}).fetchall()
+
+def get_all_exams_and_points():
+    sql = "SELECT users_summary.users_count, users_summary.total_score, users_summary.average_score, subject_exams.subject_id, subject_exams.subject_name, subject_exams.exam_id, subject_exams.exam_name, subject_exams.time_limit_minutes, exam_summary.total_questions, exam_summary.max_points \
+        FROM (SELECT s.id as subject_id, s.name as subject_name, e.id as exam_id, e.name as exam_name, e.time_limit_minutes \
+            FROM exams e, subjects s \
+            WHERE e.subject_id=s.id) as subject_exams \
+                INNER JOIN (SELECT e.id as exam_id, COUNT(eq.question_id) as total_questions, SUM(q.points) as max_points \
+                    FROM exams e \
+                        LEFT JOIN exams_questions eq ON e.id=eq.exam_id LEFT JOIN questions q ON q.id=eq.question_id GROUP BY e.id) as exam_summary ON subject_exams.exam_id=exam_summary.exam_id \
+        LEFT JOIN ( \
+            SELECT exam_id, COUNT(user_id) as users_count, SUM(total_score) as total_score, round((SUM(total_score)*1.0/COUNT(user_id)*1.0),1) as average_score \
+            FROM users_exams GROUP BY exam_id HAVING SUM(total_score) IS NOT NULL) as users_summary \
+        ON users_summary.exam_id=subject_exams.exam_id"
+    result = db.session.execute(text(sql)).fetchall()
+    return result
