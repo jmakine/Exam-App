43497cb4416d6282de3f561d8ba9c8862d875d7c
Jenni Mäkinen <jenni.makinen70@gmail.com>
2023-09-30 16:11:06 +0300

    changed "select * ..." and other small modifications

--- exams.py
+++ exams.py
@@ -9,12 +9,12 @@ def get_exams(subject_id):
             INNER JOIN (SELECT e.id as exam_id, COUNT(eq.question_id) as total_questions, SUM(q.points) as max_points FROM exams e \
                 LEFT JOIN exams_questions eq ON e.id=eq.exam_id \
                     LEFT JOIN questions q ON q.id=eq.question_id GROUP BY e.id) as exam_summary \
-            ON subject_exams.exam_id=exam_summary.exam_id"
+            ON subject_exams.exam_id=exam_summary.exam_id;"
     return db.session.execute(text(sql), {"subject_id":subject_id}).fetchall()
 
 def get_exam(exam_id):
     sql = "SELECT e.subject_id, e.id as exam_id, e.name, e.time_limit_minutes \
-        FROM exams e WHERE e.id=:exam_id"
+        FROM exams e WHERE e.id=:exam_id;"
     return db.session.execute(text(sql), {"exam_id":exam_id}).fetchone()
 
 def count_exams(subject_id):
@@ -23,20 +23,20 @@ def count_exams(subject_id):
 def add_exam(subject_id, name, timelimit):
     try:
         sql = "INSERT INTO exams (subject_id, name, time_limit_minutes) \
-            VALUES (:subject_id, :name, :timelimit)"
+            VALUES (:subject_id, :name, :timelimit);"
         db.session.execute(text(sql), {"subject_id":subject_id, "name":name, "timelimit":timelimit})
         db.session.commit()
     except:
         return False
 
 def examname_exists(name):
-    sql = "SELECT COUNT(name) FROM exams WHERE name=:name GROUP by name"
+    sql = "SELECT id FROM exams WHERE name=:name;"
     examname_exists = db.session.execute(text(sql), {"name":name}).fetchall()
     return examname_exists != []
 
 def delete_exam(id):
     try:
-        sql = "DELETE FROM exams WHERE id=:id"
+        sql = "DELETE FROM exams WHERE id=:id;"
         db.session.execute(text(sql), {"id":id})
         db.session.commit()
     except:
@@ -46,7 +46,7 @@ def get_exam_questions_and_answers(exam_id):
     sql = "SELECT e.subject_id, e.id as exam_id, e.name as exam_name, e.time_limit_minutes, eq.question_id, q.question, q.answer, q.points \
         FROM exams e LEFT JOIN exams_questions eq ON e.id=eq.exam_id \
         LEFT JOIN questions q ON q.id=eq.question_id \
-        WHERE e.id=:exam_id"
+        WHERE e.id=:exam_id;"
     return db.session.execute(text(sql), {"exam_id":exam_id}).fetchall()
 
 def exams_total_points(exam_id):
@@ -54,26 +54,26 @@ def exams_total_points(exam_id):
         FROM exams e LEFT JOIN exams_questions eq ON e.id=eq.exam_id \
         LEFT JOIN questions q ON q.id=eq.question_id \
         WHERE e.id=:exam_id \
-        GROUP BY e.id"
+        GROUP BY e.id;"
     return db.session.execute(text(sql), {"exam_id":exam_id}).fetchone()
 
 def get_available_questions(exam_id):
     exam = get_exam(exam_id)
     exam_subject = exam.subject_id
-    sql = "SELECT * FROM \
+    sql = "SELECT DISTINCT(question_id), subject_id, question, answer, points FROM \
         (SELECT q.id as question_id, q.subject_id, q.question, q.answer, q.points FROM questions q \
             LEFT JOIN exams_questions eq ON q.id=eq.question_id \
                 WHERE q.subject_id=:subject_id AND (eq.exam_id<>:exam_id OR eq.exam_id IS NULL)) as possible_questions \
         WHERE NOT EXISTS \
             (SELECT q.id as question_id FROM questions q \
                 LEFT JOIN exams_questions eq ON q.id=eq.question_id \
-                    WHERE q.subject_id=:subject_id AND (eq.exam_id=:exam_id) AND q.id=possible_questions.question_id)"
+                    WHERE q.subject_id=:subject_id AND (eq.exam_id=:exam_id) AND q.id=possible_questions.question_id);"
     return db.session.execute(text(sql), {"exam_id":exam_id, "subject_id":exam_subject}).fetchall()
 
 def add_question(question_id, exam_id):
     try:
         sql = "INSERT INTO exams_questions (exam_id, question_id) \
-            VALUES (:exam_id, :question_id)"
+            VALUES (:exam_id, :question_id);"
         db.session.execute(text(sql), {"exam_id":exam_id, "question_id":question_id})
         db.session.commit()
     except:
@@ -97,14 +97,14 @@ def get_exam_stats():
                         LEFT JOIN (SELECT e.id as exam_id, SUM(q.points) as total_points \
                             FROM exams e LEFT JOIN exams_questions eq ON e.id=eq.exam_id \
                                 LEFT JOIN questions q ON q.id=eq.question_id GROUP BY e.id) as exams_total_points \
-                                    ON exams_total_points.exam_id=e.id"
+                                    ON exams_total_points.exam_id=e.id;"
         return db.session.execute(text(sql)).fetchall()
     except:
         return False
 
 def get_timestamp_started(exam_id, user_id):
     sql = "SELECT exam_started from users_exams \
-        WHERE (user_id, exam_id) = (:user_id, :exam_id)"
+        WHERE (user_id, exam_id) = (:user_id, :exam_id);"
     timestamp = db.session.execute(text(sql), {"exam_id":exam_id, "user_id":user_id}).fetchone()
     exam_started = timestamp[0].strftime("%c")
     return exam_started
@@ -115,7 +115,7 @@ def start_exam(exam_id):
         sql = "INSERT INTO users_exams (user_id, exam_id, exam_started) \
             SELECT :user_id, :exam_id, current_timestamp \
             WHERE NOT EXISTS ( \
-                SELECT 1 FROM users_exams WHERE (user_id, exam_id) = (:user_id, :exam_id))"
+                SELECT 1 FROM users_exams WHERE (user_id, exam_id) = (:user_id, :exam_id));"
         db.session.execute(text(sql), {"exam_id":exam_id, "user_id":user_id})
         db.session.commit()
         exam_started = get_timestamp_started(exam_id, user_id)
@@ -125,7 +125,7 @@ def start_exam(exam_id):
 
 def get_timestamp_finished(exam_id, user_id):
     sql = "SELECT exam_finished from users_exams \
-        WHERE (user_id, exam_id) = (:user_id, :exam_id)"
+        WHERE (user_id, exam_id) = (:user_id, :exam_id);"
     exam_finished = db.session.execute(text(sql), {"exam_id":exam_id, "user_id":user_id}).fetchone()
     exam_finished = exam_finished[0]
     if exam_finished != None:
@@ -138,7 +138,7 @@ def end_exam(user_id, exam_id, total_score, exam_finished):
             SET exam_finished = (CASE WHEN b.count=0 THEN current_timestamp ELSE exam_finished END), \
                 total_score = (CASE WHEN b.count=0 THEN :total_score ELSE total_score END) \
             FROM (SELECT COUNT(*) as count FROM users_exams WHERE exam_id=:exam_id AND user_id=:user_id AND exam_finished IS NOT NULL) b \
-            WHERE user_id=:user_id AND exam_id=:exam_id"
+            WHERE user_id=:user_id AND exam_id=:exam_id;"
         db.session.execute(text(sql), {"exam_id":exam_id, "user_id":user_id, "total_score":total_score, "exam_finished":exam_finished})
         db.session.commit()
     except:
