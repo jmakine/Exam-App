5681f2e74d8d19a32a1d07bcd08d8f71c765eb7d
Jenni Mäkinen <jenni.makinen70@gmail.com>
2023-09-30 16:07:40 +0300

    changed "select * ..."

--- subjects.py
+++ subjects.py
@@ -2,16 +2,16 @@ from db import db
 from sqlalchemy.sql import text
 
 def get_subjects():
-    sql = "SELECT * from subjects;"
+    sql = "SELECT id, name from subjects;"
     return db.session.execute(text(sql)).fetchall()
 
 def get_subject(id):
-    sql = "SELECT * FROM subjects WHERE id=:id;"
+    sql = "SELECT id, name FROM subjects WHERE id=:id;"
     return db.session.execute(text(sql), {"id":id}).fetchone()
 
 def add_subject(name):
     try:
-        sql = "INSERT INTO subjects (name) VALUES (:name)"
+        sql = "INSERT INTO subjects (name) VALUES (:name);"
         db.session.execute(text(sql), {"name":name})
         db.session.commit()
     except:
@@ -19,13 +19,13 @@ def add_subject(name):
 
 def delete_subject(id):
     try:
-        sql = "DELETE FROM subjects WHERE id=:id"
+        sql = "DELETE FROM subjects WHERE id=:id;"
         db.session.execute(text(sql), {"id":id})
         db.session.commit()
     except:
         return False
 
 def subjectname_exists(name):
-    sql = "SELECT COUNT(name) FROM subjects WHERE name=:name GROUP by name"
+    sql = "SELECT id FROM subjects WHERE name=:name;"
     subjectname_exists = db.session.execute(text(sql), {"name":name}).fetchall()
     return subjectname_exists != []
